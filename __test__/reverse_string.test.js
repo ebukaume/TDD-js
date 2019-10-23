8e0884a62b63b610555e6aa80ea6ca908f03649e
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-10-23 15:10:48 +0100

    fix lint errors

--- __test__/reverse_string.test.js
+++ __test__/reverse_string.test.js
@@ -1,10 +1,10 @@
-import reverseString from '../lib/reverse_string.js'
+import reverseString from '../lib/reverse_string.js';
 
 it('returns the string in the reverse order', () => {
   const originalString = 'straight';
   const reversedString = reverseString(originalString);
 
-  expect(reversedString).toBe('thgiarts')
+  expect(reversedString).toBe('thgiarts');
 });
 
 it('throws an error for non string inputs', () => {
