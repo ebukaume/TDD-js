8c2d2580c0900cb142db5caa45802a069d0b59e2
nazeh <ar.nazeh@gmail.com>
2019-10-23 13:08:58 +0300

    capitalizes multiple words in a sentence

--- __test__/capitalize.test.js
+++ __test__/capitalize.test.js
@@ -1,13 +1,13 @@
 import capitalize from '../lib/capitalize.js';
 
-it('capitalize one word', () => {
+it('capitalizes one word', () => {
   const originalString = 'someString';
   const capitalizedString = capitalize(originalString);
 
   expect(capitalizedString).toBe('SomeString');
 });
 
-it('capitalize multiple words', () => {
+it('capitalizes multiple words in a sentence', () => {
   const originalString = 'firstString secondWord';
   const capitalizedString = capitalize(originalString);
 
