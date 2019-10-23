64765040152c6389be2932b75823c696f5d8f079
nazeh <ar.nazeh@gmail.com>
2019-10-23 15:37:39 +0300

    caesar cipher shifts characters, wraps from z to a and handles punctuation

--- __test__/array_analysis.test.js
+++ __test__/array_analysis.test.js
@@ -1,13 +1,13 @@
 import analyze from '../lib/array_analysis.js';
 
 it('returns an object with the min, max, length and average of the elements', () => {
-  const input = [1,2,3,4];
+  const input = [1, 2, 3, 4];
 
   const expectedObject = {
     min: 1,
     max: 4,
     length: 4,
-    average: 2.5
-  }
-  expect(analyze(input)).toEqual(expectedObject)
+    average: 2.5,
+  };
+  expect(analyze(input)).toEqual(expectedObject);
 });
